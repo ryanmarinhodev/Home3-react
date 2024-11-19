a77560f91696eda9888347db0a9dc6b0609f7de4
ryanmarinhodev <ryanmarinhodev@gmail.com>
2024-11-19 14:09:40 -0300

    carrossel pronto e responsivo

--- src/components/Carrousel/carrousel-styled.ts
+++ src/components/Carrousel/carrousel-styled.ts
@@ -15,6 +15,7 @@ export const StyledSwiper = styled(Swiper)`
     display: flex;
     justify-content: center;
     align-items: center;
+
     img {
       width: 100%;
       height: auto;
@@ -24,6 +25,10 @@ export const StyledSwiper = styled(Swiper)`
       @media (max-width: 980px) {
         margin-top: 55px;
         width: 100%;
+      }
+
+      @media (max-height: 700px) {
+        max-height: 70vh;
       }
     }
   }
@@ -39,12 +44,34 @@ export const StyledSwiper = styled(Swiper)`
 
   .swiper-button-next,
   .swiper-button-prev {
-    color: #333;
+    color: #fff;
     opacity: 0.7;
     transition: opacity 0.3s ease;
+    position: absolute;
+    top: 50%;
+
+    width: 30px;
+    height: 30px;
+    font-size: 14px !important;
 
     &:hover {
       opacity: 1;
+    }
+
+    &::after {
+      font-size: 14px !important;
+    }
+
+    @media (max-width: 768px) {
+      width: 20px;
+      height: 20px;
+      font-size: 12px !important;
+
+      &::after {
+        font-size: 12px !important;
+        position: absolute;
+        top: 40px;
+      }
     }
   }
 
