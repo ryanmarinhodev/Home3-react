d4cd3d9cd7aad6e340ef7817585e84786d6a95b5
ryanmarinhodev <ryanmarinhodev@gmail.com>
2024-11-22 11:29:02 -0300

    deixando o texto responsivo e estilizado

--- src/components/Casa-Text/casa-style.ts
+++ src/components/Casa-Text/casa-style.ts
@@ -1,17 +1,83 @@
 import styled from "styled-components";
 
-export const ImageHome = styled.img`
-  width: 850px;
-  height: auto;
-  display: flex;
-  margin: 0 auto;
-  margin-top: 10px;
+// Variáveis de estilo (se você preferir usar variáveis para cores e tamanhos)
+const colors = {
+  black: "#000000",
+  darkGray: "#1b1e23",
+  white: "#ffffff",
+};
 
-  @media (max-width: 950px) {
-    width: 400px;
+const breakpoints = {
+  large: "950px",
+  medium: "768px",
+  small: "315px",
+};
+
+export const Container = styled.div`
+  background-color: ${colors.black};
+  width: auto;
+  height: auto;
+`;
+
+export const ImageHome = styled.img`
+  width: 750px;
+  height: auto;
+  display: block;
+  margin: 10px auto 0;
+
+  @media (max-width: ${breakpoints.large}) {
+    width: 600px;
   }
 
-  @media (max-width: 768px) {
+  @media (max-width: ${breakpoints.medium}) {
     width: 300px;
+  }
+
+  @media (max-width: ${breakpoints.small}) {
+    width: 250px;
+  }
+`;
+
+export const ContainerText = styled.div`
+  background-color: ${colors.darkGray};
+  width: 700px;
+  height: 70px;
+  border-radius: 9px;
+  display: flex;
+  margin: 0 auto;
+  padding: 0 20px;
+  align-items: center;
+
+  @media (max-width: ${breakpoints.large}) {
+    width: 550px;
+  }
+
+  @media (max-width: ${breakpoints.medium}) {
+    width: 300px;
+    height: 90px;
+  }
+
+  @media (max-width: ${breakpoints.small}) {
+    width: 220px;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 19px;
+  color: ${colors.white};
+  text-align: center;
+  margin: 0;
+  width: 100%;
+
+  @media (max-width: ${breakpoints.large}) {
+    font-size: 17px;
+  }
+
+  @media (max-width: ${breakpoints.medium}) {
+    font-size: 13px;
+  }
+
+  @media (max-width: ${breakpoints.small}) {
+    font-size: 11px;
   }
 `;
--- src/components/Casa-Text/casa.tsx
+++ src/components/Casa-Text/casa.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { ImageHome } from "./casa-style";
+import { Container, ContainerText, ImageHome, Text } from "./casa-style";
 import Image from "../../assets/imagens/imageHomeCasa.png";
 
-const Casa = () => {
-  return <ImageHome src={Image} alt="Foto Casa" />;
+const Casa: React.FC = () => {
+  return (
+    <Container>
+      <ImageHome src={Image} alt="Foto Casa" />
+      <ContainerText>
+        <Text>
+          Descubra um mundo onde a tecnologia trabalha a seu favor, tornando
+          cada ação mais simples e eficiente.
+        </Text>
+      </ContainerText>
+    </Container>
+  );
 };
 
 export default Casa;
