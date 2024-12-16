0b308d547fae163c3cee0d6c242358b699283d66
ryanmarinhodev <ryanmarinhodev@gmail.com>
2024-12-16 16:26:29 -0300

    adicionando as imagens dos projetos

--- src/components/Pages/projetos.tsx
+++ src/components/Pages/projetos.tsx
@@ -1,5 +1,17 @@
+import Imagem1 from "../../assets/fotosprojetos/automacao.svg";
+import Imagem2 from "../../assets/fotosprojetos/cameras.svg";
+import Imagem3 from "../../assets/fotosprojetos/homecinema.svg";
+import Imagem4 from "../../assets/fotosprojetos/rede.svg";
+
 const Projetos = () => {
-  return <h1>Projetos</h1>;
+  return (
+    <>
+      <img src={Imagem1} alt="Imagem app" />
+      <img src={Imagem2} alt="Imagem app2" />
+      <img src={Imagem3} alt="Imagem app3" />
+      <img src={Imagem4} alt="Imagem app4" />
+    </>
+  );
 };
 
 export default Projetos;
